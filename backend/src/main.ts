0fcf26ffd043c301a42094b2b32d5b922d27143f
rubenabraham03 <151103074+rubenabraham03@users.noreply.github.com>
2025-07-02 17:47:23 -0500

    more backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
 
-async function bootstrap() {
+async function main() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  console.log(users);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
-bootstrap();
+main();
+
