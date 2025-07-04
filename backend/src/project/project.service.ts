0bfce03c1f16f4bbc96da71064e193f62597aecc
akcuhmoris <akhil.morisetty@gmail.com>
2025-07-03 23:36:44 -0500

    added crud endpoints

--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -4,19 +4,49 @@ import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async getAllProjects() {
     return this.prisma.project.findMany();
   }
 
-  async createProject(data: CreateProjectDto) {
+  async createProject(data: CreateProjectDto, userId: number) {
     return this.prisma.project.create({
       data: {
         name: data.name,
         codeUrl: data.codeUrl,
-        userId: data.userId,
+        userId: userId,
       },
     });
   }
+  async getProjectById(id: number, userId: number) {
+    return this.prisma.project.findFirstOrThrow({
+      where: {
+        id,
+        userId,
+      },
+    });
+  }
+  async deleteProject(id: number, userId: number) {
+    return this.prisma.project.deleteMany({
+      where: {
+        id,
+        userId,
+      },
+    });
+  }
+
+
+  async getProjectsForUser(userId: number) {
+    return this.prisma.project.findMany({
+      where: { userId },
+    });
+  }
+  async updateProject(id: number, userId: number, data: Partial<CreateProjectDto>) {
+    return this.prisma.project.updateMany({
+      where: { id, userId },
+      data,
+    });
+  }
+
 }
