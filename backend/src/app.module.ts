bee446d0d28c17bf0392d3c4aae2a87399a4441f
akcuhmoris <akhil.morisetty@gmail.com>
2025-07-08 02:45:24 -0500

    setting up firebase

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './project/project.module';
+import { FirebaseApp } from 'firebase/app';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, ProjectModule, AuthModule]
+  imports: [UserModule, PrismaModule, ProjectModule]
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJTi5RrkfJSus_rg4E4QUw_6UfNrghOm8",
+  authDomain: "genaifrontend.firebaseapp.com",
+  projectId: "genaifrontend",
+  storageBucket: "genaifrontend.firebasestorage.app",
+  messagingSenderId: "737633926454",
+  appId: "1:737633926454:web:ad8e08b2500fd396bec797",
+  measurementId: "G-PGFJM29B03"
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  const app = initializeApp(firebaseConfig);
+  
   app.enableCors({
     origin: [
       "http://localhost:5173"
