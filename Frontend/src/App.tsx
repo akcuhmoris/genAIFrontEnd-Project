711184927b2bf4a7c8aa9429a2a4cd730812091e
rubenabraham03 <151103074+rubenabraham03@users.noreply.github.com>
2025-07-08 13:14:54 -0500

    promptbar wo/ API call

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-import { useAuthStore } from "./store/authStore";
-import Login from "./components/Login";
-import { CodeEditor } from "./components/Editor";
-import { GrapeEditor } from "./components/GrapeEditor";
 
-export default function App() {
-  const token = useAuthStore((s) => s.token);
-=======
 import react, { useState } from 'react';
 import PromptBar from './components/PromptBar';
 import axios from 'axios';
@@ -18,7 +10,7 @@ function App() {
   const handlePrompt = async (prompt: string) => {
     setLoading(true);
     console.log('User asked:', prompt);
->>>>>>> r-promptbar
+
 
   try {
     const apiKey = 'my key'; 
@@ -43,19 +35,13 @@ function App() {
     setResponse(message);
   } catch (error) {
     console.error('API call error:', error);
-    setResponse('Sorry, something went wrong with the API call.');
+    setResponse('API call is not created yet. This is just a generated response 😁');
   } finally {
     setLoading(false);
   }
 };
 
   return (
-<<<<<<< HEAD
-    <div className="p-4 space-y-6">
-      <h1 className="text-2xl font-bold">Frontend Builder</h1>
-      <CodeEditor value="<h1>Hello</h1>" onChange={(val) => console.log(val)} />
-      <GrapeEditor />
-=======
     <div className="min-h-screen pb-20 px-6 pt-6 bg-gray-100 text-gray-90">
       <h1 className=" text-2xl font-semibold mb-4">Frontend Generator</h1>
 
@@ -65,7 +51,7 @@ function App() {
         </div>
       )}
       <PromptBar onSubmit={handlePrompt} loading={loading} />
->>>>>>> r-promptbar
+
     </div>
   );
 }
